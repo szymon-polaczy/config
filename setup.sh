4e881f6afb1d8eabc85e30dbc40c3b370ce280a4
Szymon Polaczy <s.polaczy@osomstudio.com>
2025-03-23 17:13:32 +0100

    Add TPM installation to the setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 ln -s ~/.config/.tmux ~/.tmux
 ln -s ~/.config/.tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 
